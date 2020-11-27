cf9b40adabea9e49712bc9d8a6e8d1d6664d5c8e
Christian Proske <proske@webstollen.de>
2020-11-27 16:46:45 +0100

    #11 mollie components

--- paymentmethod/CreditCard.php
+++ paymentmethod/CreditCard.php
@@ -5,11 +5,69 @@
 
 namespace Plugin\ws5_mollie\paymentmethod;
 
+use JTL\Shop;
+use Plugin\ws5_mollie\lib\MollieAPI;
 use Plugin\ws5_mollie\lib\PaymentMethod;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 class CreditCard extends PaymentMethod
 {
+    public const CACHE_TOKEN = 'creditcard:token';
+    public const CACHE_TOKEN_TIMESTAMP = 'creditcard:token:timestamp';
+
     public const METHOD = \Mollie\Api\Types\PaymentMethod::CREDITCARD;
+
+
+    public function handleAdditional(array $post): bool
+    {
+        $components = self::Plugin()->getConfig()->getValue($this->moduleID . '_components');
+        $profileId = self::Plugin()->getConfig()->getValue('profileId');
+
+        if ($components === 'N' || !$profileId || trim($profileId) === '') {
+            return parent::handleAdditional($post);
+        }
+
+        if (array_key_exists('clear', $post) && (int)$post['clear']) {
+            $this->unsetCache(self::CACHE_TOKEN)
+                ->unsetCache(self::CACHE_TOKEN_TIMESTAMP);
+        }
+
+        if ($components === 'S' && array_key_exists('skip', $post) && (int)$post['skip']) {
+            return parent::handleAdditional($post);
+        }
+
+        try {
+            $trustBadge = (bool)self::Plugin()->getConfig()->getValue($this->moduleID . '_trustBadge');
+            $locale = self::getLocale(\Session::getInstance()->getLanguage()->getIso(), \Session::getCustomer()->cLand ?? null);
+            $mode = MollieAPI::getMode();
+            $errorMessage = json_encode(self::Plugin()->getLocalization()->getTranslation('mcErrorMessage'), JSON_THROW_ON_ERROR);
+        } catch (\Exception $e) {
+            Shop::Container()->getLogService()->error($e->getMessage(), ['e' => $e]);
+            return parent::handleAdditional($post);
+        }
+
+        if (array_key_exists('cardToken', $post) && ($token = trim($post['cardToken']))) {
+            $this->addCache(self::CACHE_TOKEN, $token)
+                ->addCache(self::CACHE_TOKEN_TIMESTAMP, time() + 3600);
+            return parent::handleAdditional($post);
+        }
+
+        if (($ctTS = (int)$this->getCache(self::CACHE_TOKEN_TIMESTAMP)) && $ctTS > time()) {
+            $token = $this->getCache(self::CACHE_TOKEN);
+        }
+
+        Shop::Smarty()->assign('profileId', $profileId)
+            ->assign('trustBadge', $trustBadge ?? false)
+            ->assign('components', $components)
+            ->assign('locale', $locale ?? 'de_DE')
+            ->assign('token', $token ?? false)
+            ->assign('testMode', $mode ?? false)
+            ->assign('errorMessage', $errorMessage ?? 'Unexpected Error.')
+            ->assign('mollieLang', self::Plugin()->getLocalization()->getTranslations());
+
+        return false;
+    }
+
+
 }
