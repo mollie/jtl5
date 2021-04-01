6a91802d584d56406b81bfd432ff9c10ba210bff
Christian Proske <proske@webstollen.de>
2021-04-01 15:04:52 +0200

    fix session notice

--- frontend/hooks/131.php
+++ frontend/hooks/131.php
@@ -5,17 +5,13 @@ use Plugin\ws5_mollie\lib\Checkout\AbstractCheckout;
 use Plugin\ws5_mollie\lib\Queue;
 
 try {
-
-    if (!array_key_exists('mollie_queue_disabled', $_SESSION)) {
         require_once __DIR__ . '/../../vendor/autoload.php';
 
         ifndef('MOLLIE_QUEUE_MAX', 3);
         Queue::run(MOLLIE_QUEUE_MAX);
         AbstractCheckout::sendReminders();
-    }
 
 } catch (Exception $e) {
-    $_SESSION['mollie_queue_disabled'] = true;
     Shop::Container()->getLogService()->error($e->getMessage() . " (Trace: {$e->getTraceAsString()})");
 }
 
