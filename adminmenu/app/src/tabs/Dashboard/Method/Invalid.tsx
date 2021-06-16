fe80694a8c7cb550bad0820dc04ca06eb7efa6b9
Christian Proske <proske@webstollen.de>
2021-06-16 17:33:42 +0200

    backend usability

--- adminmenu/app/src/tabs/Dashboard/Method/Invalid.tsx
+++ adminmenu/app/src/tabs/Dashboard/Method/Invalid.tsx
@@ -2,53 +2,76 @@ import React from "react";
 import {MethodProps} from "./index";
 import {faExclamationTriangle, faTimesOctagon} from "@fortawesome/pro-solid-svg-icons";
 import {PaymentMethod2img, showMethodInfo} from "../../../helper";
-import {faCog, faInfoCircle} from "@fortawesome/pro-regular-svg-icons";
+import {
+  faCog,
+  faEnvelopeOpenDollar,
+  faEnvelopeOpenText,
+  faInfoCircle,
+  faShippingFast
+} from "@fortawesome/pro-regular-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Invalid = ({method}: { method: MethodProps }) => {
 
-    const errors : string[] = [];
-    if (!method.shipping.length) {
-        errors.push("Keine Versandarten verknüpft.")
-    }
-    if (!method.paymentMethod) {
-        errors.push("Keine Zahlungsart im Shop gefunden.")
-    }
-    if (method.paymentMethod && method.duringCheckout && !method.allowDuringCheckout) {
-        errors.push("Zahlung vor Bestellabschluss leider nicht möglich.")
-    }
+  const errors: string[] = [];
+  if (!method.shipping.length) {
+    errors.push("Keine Versandarten verknüpft.")
+  }
+  if (!method.paymentMethod) {
+    errors.push("Keine Zahlungsart im Shop gefunden.")
+  }
+  if (method.paymentMethod && method.duringCheckout && !method.allowDuringCheckout) {
+    errors.push("Zahlung vor Bestellabschluss leider nicht möglich.")
+  }
 
-    return <div key={method.mollie.id} style={{flexBasis: '33%'}}>
-        <div className="m-2 p-2 border-b">
-            {method.mollie.status !== 'activated' ?
-                <FontAwesomeIcon className={"mr-4 ml-1 cursor-help"}
-                                 icon={faTimesOctagon}
-                                 color={"red"}
-                                 onClick={() => alert('Zahlungsart bei Mollie nicht aktiv.')}
-                                 title={"Zahlungsart bei Mollie nicht aktiv."}/>
-                : <FontAwesomeIcon className={"mr-4 ml-1 cursor-help"}
-                                   icon={faExclamationTriangle}
-                                   color={"red"}
-                                   onClick={() => alert(errors.join('\n'))}
-                                   title={errors.join(' ')}/>}
+  return <div key={method.mollie.id} style={{flexBasis: '33%'}}>
+    <div className="m-2 p-2 border-b">
+      {method.mollie.status !== 'activated' ?
+          <FontAwesomeIcon className={"mr-4 ml-1 cursor-help"}
+                           icon={faTimesOctagon}
+                           color={"red"}
+                           onClick={() => alert('Zahlungsart bei Mollie nicht aktiv.')}
+                           title={"Zahlungsart bei Mollie nicht aktiv."}/>
+          : <FontAwesomeIcon className={"mr-4 ml-1 cursor-help"}
+                             icon={faExclamationTriangle}
+                             color={"red"}
+                             onClick={() => alert(errors.join('\n'))}
+                             title={errors.join(' ')}/>}
 
-            <PaymentMethod2img method={method.mollie.id}/> <span
-            className={method.mollie.status !== 'activated' ? "text-red-400" : ''}>{method.mollie.description}</span>
+      <PaymentMethod2img method={method.mollie.id}/> <span
+        className={method.mollie.status !== 'activated' ? "text-red-400" : ''}>{method.mollie.description}</span>
 
-            <FontAwesomeIcon icon={faInfoCircle} title={'Informationen anzeigen'} size={"sm"}
-                             className="ml-2 cursor-help" onClick={() => showMethodInfo(method)}/>
+      <FontAwesomeIcon icon={faInfoCircle} title={'Informationen anzeigen'} size={"sm"}
+                       className="ml-2 cursor-help" onClick={() => showMethodInfo(method)}/>
 
-            <div className="float-right">
-                {method.paymentMethod === false ? null :
-                    <a title="Einstellungen" href={method.settings}
-                       className={"ml-1"}
-                       target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faCog}/>
-                    </a>}
-            </div>
+      <div className="float-right">
 
-        </div>
+        {method.duringCheckout ?
+            <FontAwesomeIcon icon={faEnvelopeOpenDollar}
+                             className={"ml-1 cursor-help"}
+                             title={"Zahlung vor Bestellabschluss"}
+                             color={method.allowDuringCheckout ? 'green' : 'red'}/>
+            : <FontAwesomeIcon icon={faEnvelopeOpenText}
+                               className={"ml-1 cursor-help"}
+                               title={"Zahlung nach Bestellabschluss"}
+                               color={method.allowDuringCheckout ? 'blue' : 'green'}/>}
+
+        {!method.shipping || !method.shipping?.length ? <FontAwesomeIcon icon={faShippingFast} color="red"
+                                                                         title={"Mit keine Versandart verbunden."}
+                                                                         className={"ml-1 cursor-help"}/> : null}
+
+        {method.paymentMethod === false ? null :
+            <a title="Einstellungen" href={method.settings}
+               className={"ml-1"}
+               target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faCog}/>
+            </a>}
+
+
+      </div>
+
     </div>
+  </div>
 }
 
 export default Invalid;
--- adminmenu/app/src/tabs/Dashboard/Method/Valid.tsx
+++ adminmenu/app/src/tabs/Dashboard/Method/Valid.tsx
@@ -2,64 +2,82 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
 import {MethodProps} from "./index";
 import {faExclamationTriangle} from "@fortawesome/pro-solid-svg-icons";
-import {faCog, faCreditCard, faInfoCircle, faMoneyBill, faShippingFast} from "@fortawesome/pro-regular-svg-icons";
+import {
+  faCog,
+  faCreditCard,
+  faEnvelopeOpenDollar,
+  faEnvelopeOpenText,
+  faInfoCircle,
+  faMoneyBill,
+  faShippingFast
+} from "@fortawesome/pro-regular-svg-icons";
 import {PaymentMethod2img, showMethodInfo} from "../../../helper";
 
 
 const Valid = ({method}: { method: MethodProps }) => {
-    return <div key={method.mollie.id} style={{flexBasis: '33%'}}>
-        <div className="m-2 p-2 border-b">
+  return <div key={method.mollie.id} style={{flexBasis: '33%'}}>
+    <div className="m-2 p-2 border-b">
 
-            {method.paymentMethod
-            && method.duringCheckout && !method.allowDuringCheckout
-            && <FontAwesomeIcon className={"mr-4 ml-1 cursor-help"} icon={faExclamationTriangle}
-                                color={"red"}
-                                onClick={() => alert('Zahlung vor Bestellabschluss wird nicht unterstützt. Diese Zahlungsart wird nicht zur auswahl stehen.')}
-                                title={"Zahlung vor Bestellabschluss nicht unterstützt."}/>}
+      {method.paymentMethod
+      && method.duringCheckout && !method.allowDuringCheckout
+      && <FontAwesomeIcon className={"mr-4 ml-1 cursor-help"} icon={faExclamationTriangle}
+                          color={"red"}
+                          onClick={() => alert('Zahlung vor Bestellabschluss wird nicht unterstützt. Diese Zahlungsart wird nicht zur auswahl stehen.')}
+                          title={"Zahlung vor Bestellabschluss nicht unterstützt."}/>}
 
-            <PaymentMethod2img method={method.mollie.id}/> {method.mollie.description}
+      <PaymentMethod2img method={method.mollie.id}/> {method.mollie.description}
 
-            <FontAwesomeIcon icon={faInfoCircle} title={'Informationen anzeigen'} size={"sm"}
-                             className="ml-2 cursor-help" onClick={() => showMethodInfo(method)}/>
+      <FontAwesomeIcon icon={faInfoCircle} title={'Informationen anzeigen'} size={"sm"}
+                       className="ml-2 cursor-help" onClick={() => showMethodInfo(method)}/>
 
 
-            <div className="float-right">
+      <div className="float-right">
 
-                {method.components ? <>
-                    {method.components === 'S' || method.components === 'Y' ?
-                        <FontAwesomeIcon className={"ml-1 cursor-help"} icon={faCreditCard}
-                                         onClick={() => alert("Mollie Components sind aktiviert: " + (method.components === 'S' ? 'optional.' : 'obligatorisch.'))}
-                                         title={"Mollie Components enabled." + (method.components === 'S' ? ' (optional)' : ' (obligatorisch)')}
-                                         color={method.components === 'S' ? 'green' : 'blue'}/>
-                        : <FontAwesomeIcon className={"ml-1 cursor-help"} icon={faCreditCard}
-                                           onClick={() => alert("Mollie Components sind deaktiviert.")}
-                                           color={'red'}
-                                           title={'Mollie Components disabled'}/>
-                    }
-                </> : null}
-                {method.api ? (
-                    method.api === 'payment' ?
-                        <FontAwesomeIcon className={"cursor-help ml-1"} icon={faMoneyBill}
-                                         onClick={() => alert("Payment API ist aktiviert.")}
-                                         title={'Payment API'} color={"green"}/> : null
-                ) : null}
+        {method.duringCheckout ?
+            <FontAwesomeIcon icon={faEnvelopeOpenDollar}
+                             className={"ml-1 cursor-help"}
+                             title={"Zahlung vor Bestellabschluss"}
+                             color={method.allowDuringCheckout ? 'green' : 'red'}/>
+            : <FontAwesomeIcon icon={faEnvelopeOpenText}
+                               className={"ml-1 cursor-help"}
+                               title={"Zahlung nach Bestellabschluss"}
+                               color={method.allowDuringCheckout ? 'blue' : 'green'}/>}
 
-                <FontAwesomeIcon icon={faShippingFast}
-                                 onClick={() => alert(method.shipping && method.shipping?.length ? "Mit folgende Versandarten verknüpft:\n\n" + method.shipping.map((shipping: Record<string, any>) => ' - ' + shipping.cName).join("\n") : 'Mit keiner Versandart verknüpft!')}
-                                 className="cursor-help ml-1"
-                                 color={method.shipping && method.shipping?.length ? 'green' : 'red'}
-                                 title={method.shipping && method.shipping?.length ? method.shipping.map((shipping: Record<string, any>) => shipping.cName).join(', ') : 'n/a'}/>
+        {method.components ? <>
+          {method.components === 'S' || method.components === 'Y' ?
+              <FontAwesomeIcon className={"ml-1 cursor-help"} icon={faCreditCard}
+                               onClick={() => alert("Mollie Components sind aktiviert: " + (method.components === 'S' ? 'optional.' : 'obligatorisch.'))}
+                               title={"Mollie Components enabled." + (method.components === 'S' ? ' (optional)' : ' (obligatorisch)')}
+                               color={method.components === 'S' ? 'green' : 'blue'}/>
+              : <FontAwesomeIcon className={"ml-1 cursor-help"} icon={faCreditCard}
+                                 onClick={() => alert("Mollie Components sind deaktiviert.")}
+                                 color={'red'}
+                                 title={'Mollie Components disabled'}/>
+          }
+        </> : null}
+        {method.api ? (
+            method.api === 'payment' ?
+                <FontAwesomeIcon className={"cursor-help ml-1"} icon={faMoneyBill}
+                                 onClick={() => alert("Payment API ist aktiviert.")}
+                                 title={'Payment API'} color={"green"}/> : null
+        ) : null}
 
-                {method.paymentMethod === false ? null :
-                    <a title="Einstellungen" href={method.settings}
-                       className={"ml-1"}
-                       target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faCog}/>
-                    </a>}
+        <FontAwesomeIcon icon={faShippingFast}
+                         onClick={() => alert(method.shipping && method.shipping?.length ? "Mit folgende Versandarten verknüpft:\n\n" + method.shipping.map((shipping: Record<string, any>) => ' - ' + shipping.cName).join("\n") : 'Mit keiner Versandart verknüpft!')}
+                         className="cursor-help ml-1"
+                         color={method.shipping && method.shipping?.length ? 'green' : 'red'}
+                         title={method.shipping && method.shipping?.length ? method.shipping.map((shipping: Record<string, any>) => shipping.cName).join(', ') : 'n/a'}/>
 
-            </div>
-        </div>
+        {method.paymentMethod === false ? null :
+            <a title="Einstellungen" href={method.settings}
+               className={"ml-1"}
+               target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faCog}/>
+            </a>}
+
+      </div>
     </div>
+  </div>
 }
 
 export default Valid;
