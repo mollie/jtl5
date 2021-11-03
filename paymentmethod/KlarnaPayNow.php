e06bfecbf2598899f0c6a8aaf5f19bdbc0e60ea7
Christian Proske <proske@webstollen.de>
2021-11-03 16:06:42 +0100

    Added Klarna Pay Now Class

---
+++ paymentmethod/KlarnaPayNow.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright 2021 WebStollen GmbH
+ * @link https://www.webstollen.de
+ */
+
+namespace Plugin\ws5_mollie\paymentmethod;
+
+use JTL\Checkout\Bestellung;
+use Plugin\ws5_mollie\lib\PaymentMethod;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class KlarnaPayNow extends PaymentMethod
+{
+    public const ALLOW_PAYMENT_BEFORE_ORDER = true;
+
+    public const METHOD = \Mollie\Api\Types\PaymentMethod::KLARNA_PAY_NOW;
+
+    public function getPaymentOptions(Bestellung $order, $apiType): array
+    {
+        return [];
+    }
+}
