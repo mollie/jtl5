b4655fb844c6b06cccf6cb37812b348d5831f22c
Johannes <graf@webstollen.de>
2024-09-05 10:08:27 +0200

    remove src

--- adminmenu/app/src/context/MollieContext.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { UseMollieReturn } from '../hooks/useMollie'
-
-const MollieContext = React.createContext<UseMollieReturn>({} as UseMollieReturn)
-export default MollieContext
